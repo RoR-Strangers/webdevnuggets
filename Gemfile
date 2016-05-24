28c86b281f5d3bd165a2caca39ca6c2cb4cb50fa
Exellin <carnegieshawn@yahoo.ca>
2016-05-24 05:00:54 +0000

    added syntax highlighting

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'devise'
 gem 'jquery-ui-rails'
 gem 'greensock-rails'
 gem 'redcarpet'
+gem 'pygments.rb'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,15 @@ module ApplicationHelper
   def require_admin
     current_user.nil? ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
   end
-  
+
+  class HTMLwithPygments < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
+    end
+  end
+    
   def markdown(content)
-    renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true,)
+    renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true,)
     options = {
       autolink: true,
       no_intra_emphasis: true,
